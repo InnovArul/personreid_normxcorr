e58f3197f8b6a70de12c064eb185ddee019d018f
Arul <arul.csecit@ymail.com>
2016-11-27 18:55:00 +0530

    cuhk01 test100 preprocessing scripts are added

--- datasets/cuhk01_test100/randomSplit_test100.m
+++ datasets/cuhk01_test100/randomSplit_test100.m
@@ -2,7 +2,7 @@
 % into 100 identities of labeled_testsets & detected_testsets
 
 % get the names of folder
-rootsource = 'train_871';
+rootsource = 'train';
 dirFolders = dir(rootsource);
 foldernames = extractfield(dirFolders, 'name');
 
@@ -16,19 +16,16 @@ randomorder = randperm(size(foldernames, 1));
 foldernames = foldernames(randomorder, :);
 
 testsetfolders = foldernames(1:100, :);
-
+ 
 % take first 100 folder names and copy it to testsets
-%for index = 1:length(directories)
-    dirname = 'test_100';
-    
-    % create testset folder
-    %testsetname = strcat(dirname, '_testsets');
-    %mkdir(testsetname);
-    
-    for testsetIndex = 1:length(testsetfolders)
-       source = strcat(rootsource, '/', testsetfolders(testsetIndex));
-       disp(source)
-       movefile(source{:}, dirname, 'f'); 
-    end
-    
-%end
+dirname = 'test';
+%create a folder with name as prefix
+if(~exist(dirname, 'file'))
+  mkdir(dirname); 
+end
+
+for testsetIndex = 1:length(testsetfolders)
+   source = strcat(rootsource, '/', testsetfolders(testsetIndex));
+   disp(strcat(source, {' --> '}, dirname))
+   movefile(source{:}, dirname, 'f'); 
+end
